d720679250fab5d9ef4ea619314559cf1082ef40
sunhui <2923132761@qq.com>
2019-11-12 18:35:56 +0800

    feat:factory

--- fectorymethod.js
+++ fectorymethod.js
@@ -1,3 +1,38 @@
 /**
  * 工厂模式
  */
+class Car {
+  constructor(options) {
+    this.size = options.size || 'small'
+    this.color = options.color || 'red'
+  }
+}
+
+class Truck {
+  constructor(options) {
+    this.size = options.size || 'big'
+    this.color = options.color || 'black'
+  }
+}
+
+class VehicleFactory {
+  constructor () {
+    this.vehicleClass = Car
+  }
+
+  createVehicle(options) {
+    if (options.vehicleType === 'car') {
+      this.vehicleClass = Car
+    } else {
+      this.vehicleClass = Truck
+    }
+
+    return new this.vehicleClass(options)
+  }
+}
+
+const vehicle = new VehicleFactory()
+const myCar = vehicle.createVehicle({
+  vehicleType: 'car',
+  size: 'middle'
+})
